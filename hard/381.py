7f1c526ac90b6b1173e50baf57795fccbdee9837
huxianglong <hxianglong@gmail.com>
2018-09-17 18:06:44 -0400

    almost successful

--- hard/381.py
+++ hard/381.py
@@ -1,121 +1,108 @@
 import random
-class Node:
-    def __init__(self, size):
-        self.number = 1
-        self.location = {1:size}
-        self.order = {size:1}
-
-    # def remove(self):
-    #     self.
-
 class RandomizedCollection:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.insert_table = {}
-        self.random_table = {}
+        self.array = [] # (val, index of prev same val)
+        self.hash = {} # (val, array_index)
         self.size = 0
 
     def insert(self, val):
-        """
-        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
-        :type val: int
-        :rtype: bool
-        """
-        self.size += 1
-        if val in self.insert_table:
-            # add the size of duplicate values
-            self.insert_table[val][0] += 1
-            size = self.insert_table[val][0]
-            # use hash function to remember the location
-            self.insert_table[val][1][size] = self.size
-            self.insert_table[val][2][self.size] = size
-            # add value to random table
-            self.random_table[self.size] = val
+        if val in self.hash:
+            # self.array[self.size] = (val, self.hash[val])
+            self.array.append((val, self.hash[val]))
+            self.hash[val] = self.size
+            self.size += 1
             return False
         else:
-            # self.insert_table[val] = {number:1, location:{1:self.size}, order:{self.size:1}}
-            self.insert_table[val] = [1, {1:self.size}, {self.size:1}]
-            self.random_table[self.size] = val
+            # self.array[self.size] = (val, -1)
+            self.array.append((val, -1))
+            self.hash[val] = self.size
+            self.size += 1
             return True
 
     def remove(self, val):
-        """
-        Removes a value from the collection. Returns true if the collection contained the specified element.
-        :type val: int
-        :rtype: bool
-        """
-        if val in self.insert_table:
-            size_remove = self.insert_table[val][0]
-            location_map = self.insert_table[val][1]
-            last_val = self.random_table[self.size]
-            if last_val == val:
-                ord_remove = self.insert_table[val][2][self.size]
-                self.insert_table[val][2].pop(self.size, None)
-                location_map.pop(ord_remove, None)
-                self.random_table.pop(self.size, None)
-                self.size -= 1
-            else:
-                if size_remove == 1:
-                    loc_remove = location_map[1]
-                    map_replace = self.insert_table[last_val][2]
-                    ord_replace = map_replace[self.size]
-                    self.insert_table[last_val][1][ord_replace] = loc_remove
-                    map_replace.pop(self.size, None)
-                    map_replace[loc_remove] = ord_replace
-
-                    self.insert_table.pop(val, None)
-
-                    self.random_table[loc_remove] = self.random_table[self.size]
-                    self.random_table.pop(self.size, None)
-                    self.size -= 1
+        if val in self.hash:
+            index  = self.hash[val]
+            node = self.array[index]
+            last_node = self.array[self.size - 1]
+            last_val = last_node[0]
+            # this is no repetitious element
+            if node[1] == -1:
+                # this means we are removing the last element, there is no hole
+                if last_node == node:
+                    # delete hash table entry
+                    self.hash.pop(val, None)
+                    # self.array[self.size - 1] = ()
                 else:
-                    loc_remove = location_map[size_remove]
-                    map_replace = self.insert_table[last_val][2]
-                    ord_replace = map_replace[self.size]
-                    self.insert_table[last_val][1][ord_replace] = loc_remove
-                    map_replace.pop(self.size, None)
-                    print(self.insert_table[last_val][1])
-                    print(self.insert_table[last_val][2])
-                    map_replace[loc_remove] = ord_replace
-
-                    self.insert_table[val][0] -= 1
-                    self.insert_table[val][1].pop(size_remove, None)
-                    self.insert_table[val][2].pop(loc_remove, None)
-
-                    self.random_table[loc_remove] = self.random_table[self.size]
-                    self.random_table.pop(self.size, None)
-                    self.size -= 1
+                    # change the hash table entry
+                    self.hash[last_val] = index
+                    # change the array
+                    self.array[index] = last_node
+                    # delete hash table entry
+                    self.hash.pop(val, None)
+                    # self.array[self.size - 1] = ()
+                    # self.size -= 1
+            else:
+                if last_node == node:
+                    self.hash[last_val] = node[1]
+                    # delete hash table entry
+                    # self.array[self.size - 1] = ()
+                    # self.size -= 1
+                else:
+                    if node[1] == self.size - 1:
+                        self.hash[last_val] = index
+                        self.array[index] = last_node
+                    else:
+                        # change the hash table entry
+                        self.hash[last_val] = index
+                        self.hash[val] = node[1]
+                        # change the array
+                        self.array[index] = last_node
+                        # self.array[self.size - 1] = ()
+                        # self.size -= 1
+            self.array.pop()
+            self.size -= 1
             return True
         else:
             return False
 
-
     def getRandom(self):
-        """
-        Get a random element from the collection.
-        :rtype: int
-        """
-        return self.random_table[random.randint(1, self.size)]
+        return self.array[random.randint(0, self.size - 1)][0]
 
 if __name__ == "__main__":
     ran = RandomizedCollection()
     command = ["RandomizedCollection","insert","insert","insert","insert","insert","insert","remove","remove","remove","remove","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom"]
     vals = [[],[1],[1],[2],[1],[2],[2],[1],[2],[2],[2],[],[],[],[],[],[],[],[],[],[]]
+    command = ["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+    vals = [[], [1], [1], [2], [], [1], []]
+    command = ["RandomizedCollection", "insert", "insert", "insert", "insert", "insert", "insert", "remove", "remove", "remove",
+     "remove", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom",
+     "getRandom", "getRandom"]
+    vals = [[], [10], [10], [20], [20], [30], [30], [10], [10], [30], [30], [], [], [], [], [], [], [], [], [], []]
     for i in range(1, len(vals)):
         com = command[i]
-        val = vals[i][0]
+        val = vals[i]
+        if len(val) != 0:
+            val = val[0]
         if com == "insert":
             # if val[0] == 13:
                 #print("Error here.")
+            print("Before insert {}".format(val))
+            print(ran.hash)
+            print(ran.array)
             ran.insert(val)
-            print(ran.insert_table)
-            print(ran.random_table)
+            print("After insert {}".format(val))
+            print(ran.hash)
+            print(ran.array)
+            print('-' * 30)
         if com == "remove":
+            print("Before remove {}".format(val))
+            print(ran.hash)
+            print(ran.array)
             ran.remove(val)
-            print(ran.insert_table)
-            print(ran.random_table)
+            print("After remove {}".format(val))
+            print(ran.hash)
+            print(ran.array)
+            print('-' * 30)
                 # print("-" * 30)
                 # print("ERROR")
                 #
