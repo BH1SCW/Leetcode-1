241b394c9cdeb754b788087d03d082581aba128e
hxianglong <hxianglong@gmail.com>
2019-02-05 16:32:56 -0500

    140 aced, 100% faster, change to dfs

--- hard/140.py
+++ hard/140.py
@@ -7,23 +7,27 @@ class Solution:
         for w in wordDict:
             n = max(len(w), n)
             dict.add(w)
-        def parse(sentence, previous):
+        def bfs(sentence):
             # print("sentence: {}\tprevious: {}".format(sentence, previous))
             if not sentence:
-                final.append(previous[:-1])
+                return [""]
+            if sentence in searched:
+                return searched[sentence]
+            result = []
             for i in range(1, min(n + 1, len(sentence) + 1)):
                 if sentence[:i] in dict:
-                    parse(sentence[i:], previous + sentence[:i] + " ")
-        parse(s, "")
-        return final
+                    result += [sentence[:i] + " " + r if r else sentence[:i] + r for r in bfs(sentence[i:])]
+            searched[sentence] = result
+            return result
+        return bfs(s)
 
 if __name__ == '__main__':
     s = "catsanddog"
     wordDict = ["cat", "cats", "and", "sand", "dog"]
-    s = "pineapplepenapple"
-    wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
-    s = "catsandog"
-    wordDict = ["cats", "dog", "sand", "and", "cat"]
+    # s = "pineapplepenapple"
+    # wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+    # s = "catsandog"
+    # wordDict = ["cats", "dog", "sand", "and", "cat"]
     sol = Solution()
     print(sol.wordBreak(s, wordDict))
 
