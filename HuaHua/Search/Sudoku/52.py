708758cc509ad071014eb46a1552a595231b67f2
hxianglong <hxianglong@gmail.com>
2019-03-25 18:44:04 -0400

    change parameter change, stll 72%

--- HuaHua/Search/Sudoku/52.py
+++ HuaHua/Search/Sudoku/52.py
@@ -5,19 +5,22 @@ class Solution:
         diags1 = [0] * (2 * n - 1)
         diags2 = [0] * (2 * n - 1)
         board = [['.'] * n for _ in range(n)]
-        def dfs(i, ans):
+        ans = 0
+        def dfs(i):
             if i == n:
                 # ans.append([''.join(ls) for ls in board])
-                return ans + 1
+                nonlocal ans
+                ans += 1
+                return
             for j in range(n):
                 di1 = i + j
                 di2 = n - 1 - i + j
                 if (not cols[i] and not rows[j] and not diags1[di1] and not diags2[di2]):
                     cols[i], rows[j], diags1[di1], diags2[di2], board[i][j] = 1, 1, 1, 1, 'Q'
-                    ans = dfs(i + 1, ans)
+                    dfs(i + 1)
                     cols[i], rows[j], diags1[di1], diags2[di2], board[i][j] = 0, 0, 0, 0, '.'
-            return ans
-        return dfs(0, 0)
+        dfs(0)
+        return ans
 
 if __name__ == '__main__':
     sol = Solution()
