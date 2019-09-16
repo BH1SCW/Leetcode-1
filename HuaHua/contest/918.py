c7fb02185542493cd24f25cf152f54a91197a4f3
hxianglong <xianglong@onai.com>
2019-09-16 00:29:16 -0400

    solved but slow, need to improve

--- HuaHua/contest/918.py
+++ HuaHua/contest/918.py
@@ -8,47 +8,38 @@ class Solution:
             l = len(arr)
             # starts with first element
             v0 = [arr[0]] + [0] * (l - 1)
+            m0 = [arr[0]] + [0] * (l - 1)
             for i in range(1, l):
                 v0[i] = v0[i - 1] + arr[i]
+                m0[i] = max(m0[i - 1], v0[i])
             # ends with last element
             v1 = [0] * (l - 1) + [arr[-1]]
             for i in range(l - 2, -1, -1):
                 v1[i] = v1[i + 1] + arr[i]
             # store
-            m = arr[0]
-            T = [[0] * l for i in range(l)]
-            for i in range(l):
-                for j in range(i, -1, -1):
-                    if i == j:
-                        T[i][j] = s
-                    else:
-                        T[i][j] = v1[i] + v0[j]
-                    if T[i][j] > m:
-                        m = T[i][j]
+            m = A[0]
+            for i in range(1, l):
+                # m1 = max(max(v0[:i]) + v1[i], m)
+                m = max(m0[i - 1] + v1[i], m)
             return m
         def helper(A):
             l = len(A)
-            T = [[0] * l for i in range(l)]
-            m = A[0]
-            for i in range(0, l):
-                for j in range(i, -1, -1):
-                    if i == j:
-                        T[i][j] = A[j]
-                    else:
-                        T[i][j] = T[i][j + 1] + A[j]
-                    if T[i][j] > m:
-                        m = T[i][j]
-            return m
+            T = [0] * l
+            T[0] = A[0]
+            for i in range(1, l):
+                T[i] = max(T[i - 1] + A[i], A[i])
+            return max(T)
         return max(helper(A), cross(A))
 
 
 if __name__ == '__main__':
     sol = Solution()
-    s = [2,-2,2,7,8,0]
     s = [-2,-3,-1]
     s = [3,-2,2,-3]
     s = [5,-3,5]
     s = [1, -2, 3, -2]
     s = [3,-1,2,-1]
+    s = [2,-2,2,7,8,0]
+    s = [-2,4,-5,4,-5,9,4]
     print(sol.maxSubarraySumCircular(s))
 
